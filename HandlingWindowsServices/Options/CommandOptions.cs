53e599a9ba509efe7b0421addd2748a69f5f1be3
stescobedo92 <stescobedo.31@gmail.com>
2023-07-03 21:55:57 -0600

    Add command definition

---
+++ HandlingWindowsServices/Options/CommandOptions.cs
@@ -0,0 +1,22 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandlingWindowsServices.Options
+{
+    public class CommandOptions
+    {
+        [Option('c', "create", Required = false, HelpText = "Create a new service")]
+        public bool CreateService { get; set; }
+
+        [Option('r', "read", Required = false, HelpText = "Read data from a service.")]
+        public string ReadDataFromService { get; }
+
+        [Option('r', "remove", Required = false, HelpText = "Remove service")]
+        public bool RemoveService { get; set; }
+    }
+}
--- HandlingWindowsServices/Program.cs
+++ HandlingWindowsServices/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace HandlingWindowsServices
+﻿using CommandLine;
+using HandlingWindowsServices.Options;
+
+namespace HandlingWindowsServices
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            CommandLine.Parser.Default.ParseArguments<CommandOptions>(args).WithParsed(RunOptions).WithNotParsed(HandleParseError);
+        }
+
+        static void RunOptions(CommandOptions opts)
+        {
+            //handle options
+        }
+        static void HandleParseError(IEnumerable<Error> errs)
+        {
+            //handle errors
         }
     }
 }
